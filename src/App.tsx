80859631060cbfe400fc1074295756fbd117e734
HiImMars <fedorenkorostyslav@gmail.com>
2024-01-23 23:25:22 +0200

    header completely done

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import Header from "./layouts/Header";
+
 function App() {
-  return <div className="max-h-screen"></div>;
+  return (
+    <div className="max-h-screen flex flex-col">
+      <Header />
+      <div>2</div>
+    </div>
+  );
 }
 
 export default App;
