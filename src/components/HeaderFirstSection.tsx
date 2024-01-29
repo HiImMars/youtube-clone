731c73af07b8772dadf829a37cdf782b8e78f7bf
HiImMars <fedorenkorostyslav@gmail.com>
2024-01-30 00:26:45 +0200

    done

---
+++ src/components/HeaderFirstSection.tsx
@@ -0,0 +1,30 @@
+import { Menu } from "lucide-react";
+import { useSidebarContext } from "../contexts/SidebarContext";
+import { Button } from "./Button";
+import logo from "../assets/logo.svg";
+
+type HeaderFirstSectionProps = {
+  hidden?: boolean;
+};
+
+export const HeaderFirstSection = ({
+  hidden = false,
+}: HeaderFirstSectionProps) => {
+  const { toggle } = useSidebarContext();
+
+  return (
+    <div
+      className={`gap-4 items-center flex-shrink-0 ${
+        hidden ? "hidden" : "flex"
+      }`}
+    >
+      <Button onClick={toggle} variant="ghost" size="icon">
+        <Menu />
+      </Button>
+      <a href="/" className="flex items-center justify-center gap-1">
+        <img src={logo} alt="logo" className="h-6" />
+        <span>FlickFrame</span>
+      </a>
+    </div>
+  );
+};
